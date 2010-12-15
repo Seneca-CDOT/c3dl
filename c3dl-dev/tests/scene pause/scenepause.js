5add780b7296458d24b1a011fd66e32c769d5bad
Matt <carnage6000@gmail.com>
2010-12-15 14:20:37 -0500

    scene pause test added

--- c3dl-dev/tests/scene pause/scenepause.js
+++ c3dl-dev/tests/scene pause/scenepause.js
@@ -1,6 +1,6 @@
 c3dl.addMainCallBack(canvasMain, "test");
 c3dl.addModel("duck.dae");
-var timesincelastchange=0;
+var timeSinceLastChange=0;
 var duck;
 var scn;
 function canvasMain(canvasName){
@@ -12,23 +12,48 @@ function canvasMain(canvasName){
     scn.init(canvasName);
     if(renderer.isReady() )
     {
-        
         duck = new c3dl.Collada();
         duck.init("duck.dae");
         duck.setAngularVel(new Array(0.0, -0.001, 0.0));
+        duck.setPosition([300,0,0]);
         scn.addObjectToScene(duck);
         var cam = new c3dl.FreeCamera();
         cam.setPosition(new Array(200.0, 300.0, 1000.0));
         cam.setLookAtPoint(new Array(0.0, 0.0, 0.0));
         scn.setCamera(cam);
+        scn.setUpdateCallback(test);
         scn.startScene();
+       
     }
 }
 
-  function pause() {
-    scn.pauseScene();
-
-  }
-  function unpause() {
-    scn.unpauseScene();
-  }
+function pauseR() {
+  scn.pauseSceneRender();
+}
+function unpauseR() {
+  scn.unpauseSceneRender();
+}
+function pauseU() {
+  scn.pauseSceneUpdate();
+}
+function unpauseU() {
+  scn.unpauseSceneUpdate();
+}
+function pause() {
+  scn.pauseScene();
+}
+function unpause() {
+  scn.unpauseScene();
+}
+function test(time) {
+  timeSinceLastChange += time; 
+  document.getElementById('res').innerHTML = (timeSinceLastChange/1000).toFixed(2) + " sec";
+}
+function addDuck() {
+  oldPos = duck.getPosition();
+  duck = new c3dl.Collada();
+  duck.init("duck.dae");
+  duck.setPosition([oldPos[0]-150,oldPos[1],oldPos[2]]);
+  duck.setAngularVel(new Array(0.0, -0.001, 0.0));
+  scn.addObjectToScene(duck);
+}
