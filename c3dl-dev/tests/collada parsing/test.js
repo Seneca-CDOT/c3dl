036844f62f7919e6044456762caeba34c6db18df
peter.callaghan@senecac.on.ca <peter.callaghan@senecac.on.ca>
2011-05-04 13:43:58 -0400

    Added regular expressions to mergeChildData function of colladaLoader to make file parsing more robust.

---
+++ c3dl-dev/tests/collada parsing/test.js
@@ -0,0 +1,31 @@
+// This test consists of a duck slowly rotating in
+//front of the camera.  If parsing of the
+//float_array occurs correctly, the duck shows up
+//normally.  If anything is wrong, the duck fails
+//to show up, or shows up covered in jagged points.
+
+c3dl.addMainCallBack(canvasMain, "test");
+c3dl.addModel("duck.dae");
+var duck;
+
+function canvasMain(canvasName){
+
+ scn = new c3dl.Scene();
+ scn.setCanvasTag(canvasName);
+ renderer = new c3dl.WebGL();
+ renderer.createRenderer(this);
+ scn.setRenderer(renderer);
+ scn.init(canvasName);
+ if(renderer.isReady() )
+ {
+  duck = new c3dl.Collada();
+  duck.init("duck.dae");
+  duck.setAngularVel(new Array(0.0, 0.001, 0.0));
+  scn.addObjectToScene(duck);
+  var cam = new c3dl.FreeCamera();
+  cam.setPosition(new Array(200.0, 300.0, 500.0));
+  cam.setLookAtPoint(new Array(0.0, 0.0, 0.0));
+  scn.setCamera(cam);
+  scn.startScene();
+ }
+}
